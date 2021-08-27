4ab0d7ac8ae5f041ee104b4c0e025dcea26b7c04
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-08-27 13:06:42 +0200

    reajustes

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-// arrancar la app
+// Arrancar la app
 app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
